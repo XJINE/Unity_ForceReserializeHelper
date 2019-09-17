ded50ca6428b49fec2d28fc2391ae503eb5a05aa
XJINE <xjine.andorelseif@gmail.com>
2019-09-17 20:30:02 +0900

    First commit.

---
+++ Assets/Packages/ForceReserializeHelper/Editor/ForceReserializeHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ForceReserializeHelper : EditorWindow
+{
+    public ForceReserializeAssetsOptions option = ForceReserializeAssetsOptions
+                                                 .ReserializeAssetsAndMetadata;
+
+    [MenuItem("Custom/ForceReserializeHelper")]
+    static void Init()
+    {
+        EditorWindow.GetWindow<ForceReserializeHelper>("ForceReserializeHelper");
+    }
+
+    protected void OnGUI()
+    {
+        GUIStyle marginStyle = GUI.skin.label;
+                 marginStyle.wordWrap = true;
+                 marginStyle.margin = new RectOffset(5, 5, 5, 5);
+
+        EditorGUILayout.LabelField("Reserialize Option");
+
+        this.option = (ForceReserializeAssetsOptions)
+                       EditorGUILayout.EnumPopup(this.option);
+
+        if (GUILayout.Button("Click to Reserialize"))
+        {
+            ForceReserialize();
+        }
+    }
+
+    protected virtual void ForceReserialize()
+    {
+        List<string> targets = new List<string>();
+        targets.AddRange(AssetDatabase.GetAllAssetPaths());
+
+
+        foreach (var target in targets) { Debug.Log(target); }
+
+        AssetDatabase.ForceReserializeAssets(targets, this.option);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Unity_ForceReserializeHelper
+
+<img src="https://github.com/XJINE/Unity_ForceReserializeHelper/blob/master/Screenshot.png" width="100%" height="auto" />
+
+Helper tool for ForceReserialize assets.
+This is useful when you use ``FormerlySerializedAsAttribute.``
+
+## Import to Your Project
+
+You can import this asset from UnityPackage.
+
+- [ForceReserializeHelper.unitypackage](https://github.com/XJINE/Unity_ForceReserializeHelper/blob/master/ForceReserializeHelper.unitypackage)
+
+## How to Use
+
+1. Open helper window from Menu > Custom > ForceReserializeHelper.
+2. Select ForceReserialize option.
+3. Click the "Click to Reserialize" button.
